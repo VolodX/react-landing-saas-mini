cf252e7c3586d2295402d63d5f939484b4ab1c5c
Volodymyr Brezitskyi <118307273+VolodX@users.noreply.github.com>
2025-07-03 13:39:13 +0300

    feat(ui): add Brands section and shared Title component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {Layout} from './components/Layout';
+import { Brands } from './components/sections/Brands';
 import {Hero} from './components/sections/Hero';
 
 function App() {
   return (
     <Layout title="EdgeAI">
       <Hero />
+			<Brands />
     </Layout>
   );
 }
